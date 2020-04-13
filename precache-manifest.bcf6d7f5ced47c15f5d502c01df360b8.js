ca50d9023c30ec1ac99e5a850563495add2e4ab8
rob <rosborn92@gmail.com>
2020-04-12 21:22:59 -0700

    Updates

---
+++ precache-manifest.bcf6d7f5ced47c15f5d502c01df360b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ade4c091d5a8efd533472d5ce1f07c8",
+    "url": "/rosborn92/my_portfolio/index.html"
+  },
+  {
+    "revision": "75818ae2c30c98eb1f87",
+    "url": "/rosborn92/my_portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1fe746691870e0b838cb",
+    "url": "/rosborn92/my_portfolio/static/js/2.bfa75db8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/rosborn92/my_portfolio/static/js/2.bfa75db8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75818ae2c30c98eb1f87",
+    "url": "/rosborn92/my_portfolio/static/js/main.160a8afb.chunk.js"
+  },
+  {
+    "revision": "525b90af6d5eafa9d0ec",
+    "url": "/rosborn92/my_portfolio/static/js/runtime-main.5e2c08c2.js"
+  }
+]);
